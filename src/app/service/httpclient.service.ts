e9dcab6eca79ff573d9d759b6e7b31ac67b77a16
shebinjm <shebinjm@gmail.com>
2019-04-29 01:50:53 +0530

    angular 7 employee portal

---
+++ src/app/service/httpclient.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export class Employee{
+  constructor(
+    public empId:string,
+    public name:string,
+    public designation:string,
+  ) {}
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpClientService {
+
+  constructor(
+    private httpClient:HttpClient
+  ) { 
+     }
+
+     getEmployees()
+  {
+    return this.httpClient.get<Employee[]>('http://localhost:8080/employee/getall');
+  }
+
+  public deleteEmployee(employee) {
+    return this.httpClient.delete<Employee>("http://localhost:8080/employee" + "/delete/"+ employee.empId);
+  }
+
+  public createEmployee(employee) {
+    return this.httpClient.post<Employee>("http://localhost:8080/employee/create", employee);
+  }
+}
